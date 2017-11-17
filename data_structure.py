52611080d6d86a2c4bceb202c61a836cb1d73c5e
linzhulz <jolinchuh@gmail.com>
2017-11-17 12:06:39 -0800

    CLRS solutions

---
+++ data_structure.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+class Queue:
+    def _init_(self, size):
+        self.size = size
+        self.head = 0
+        self.tail = 0
+
+    def enqueue(self, num):
+        if self.head ==
+
